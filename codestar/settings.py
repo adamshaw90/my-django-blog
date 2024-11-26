cdf3feb37b75e8b8020cc4b3648884aa7b6b0e02
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-11-26 11:19:17 +0000

    Change debug value to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-adamshaw90-mydjangoblog-ebm2j01zpwz.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-adamshaw90-mydjangoblog-yqgpfzub9oh.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
