39722f8895e25683ac943bd063ce0945e4bcf529
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-11-27 10:12:51 +0000

    Re-install packages

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-adamshaw90-mydjangoblog-yqgpfzub9oh.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-adamshaw90-mydjangoblog-kfpdlpuq20u.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
